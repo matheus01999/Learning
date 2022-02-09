8834546dcb9c013dbbe90495ecaf2f6d14e0fd16
Matheus Oliveira <matheus.rocha@eletricapj.com.br>
2022-02-09 16:32:24 -0300

    Primitivos: Numbers

--- Step1/Javascript/type-number.js
+++ Step1/Javascript/type-number.js
@@ -12,3 +12,13 @@
  // Ponto Flutuante
 
  const numeroPontoFlutuante = 3.3;
+ const pontoFlutuanteSemZero =.5;
+
+ const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+ console.log(novaOperacao);
+
+ //NaN -> Not a Number (não é um numero)
+
+ const alura = "Alura";
+ console.log(alura * primeiroNumero)
