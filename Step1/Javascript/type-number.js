9bd9382c88b3e2cf277c0ef7c8cd7b3d1984f7ea
Matheus Oliveira <matheus.rocha@eletricapj.com.br>
2022-02-09 16:11:55 -0300

    Tipos Primitivos: Numbers

---
+++ Step1/Javascript/type-number.js
@@ -0,0 +1,14 @@
+// Tipo number 
+
+ const meuNumero = 3;
+
+ const primeiroNumero = 1;
+ const segundoNumero = 2;
+
+ const operacaoMatematica = primeiroNumero + segundoNumero;
+
+ console.log(operacaoMatematica);
+
+ // Ponto Flutuante
+
+ const numeroPontoFlutuante = 3.3;
