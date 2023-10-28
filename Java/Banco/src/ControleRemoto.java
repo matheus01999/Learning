a244e608e46a9d0aae64996706162720b61ee8bc
Matheus Rocha <matheus.rocha01999@outlook.com>
2023-10-27 21:57:28 -0300

    fechando menu corrigido

--- Java/Banco/src/ControleRemoto.java
+++ Java/Banco/src/ControleRemoto.java
@@ -59,7 +59,7 @@ public class ControleRemoto  implements Controlador{
           for (int i = 0; i <= this.getVolume(); i+=10){
                System.out.print("=");
           }
-
+          System.out.println("");
      }
 
      @Override
